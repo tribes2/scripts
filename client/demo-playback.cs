034c55a0df3f7dbbb61295300373414691f62a31
loop <126372784+tribes2@users.noreply.github.com>
2025-10-04 17:06:22 +0100

    Add demo-playback

---
+++ client/demo-playback.cs
@@ -0,0 +1,19 @@
+// #autoload
+// #name = demo-playback
+// #date = October 4th, 2025
+// #author = loop
+// #warrior = looop
+// #email = loop@tribesforever.com
+// #description = Various fixes for demo playback issues. 1) Command Circuit always being shown (recorder was using a minimap / engineer hud)
+
+package BiteMyShinyMetalAss {
+	function Canvas::setContent(%this,%canvas)
+	{
+		if(isPlayingDemo() && %canvas $= "CommanderMapGui") {
+            return;
+        }
+		parent::setContent(%this,%canvas);
+	}
+}
+
+activatePackage(BiteMyShinyMetalAss);
