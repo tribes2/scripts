f693cae5fd07fc20cb79fdb759743f24c9f7a781
loop <126372784+tribes2@users.noreply.github.com>
2025-10-14 11:30:31 +0100

    Add ignore-server to prevent IP leaks to unsavoury characters

---
+++ client/ignore-server.cs
@@ -0,0 +1,30 @@
+// #autoload
+// #name = ignore-server
+// #date = October 14th, 2025
+// #author = loop
+// #warrior = looop
+// #email = loop@tribesforever.com
+// #description = Prevent Tribes 2 from querying a server based on a partial IP match. Compatible with RC2a and QoL
+
+$ignoredServer = "0.0.0.0"; // Replace me
+
+package ServerQueryIgnore {
+    function TNbite::onLine(%this, %line) {
+        if (strpos(%line, $ignoredServer) != -1) {
+            echo("Suppressing query for " @ $ignoredServer);
+            return;
+        }
+        parent::onLine(%this, %line);
+    }
+
+    function ServerList::onLine(%this, %line) {
+        if (strpos(%line, $ignoredServer) != -1) {
+            echo("Suppressing query for " @ $ignoredServer);
+            return;
+        }
+        parent::onLine(%this, %line);
+    }
+};
+
+echo("Ignoring server " @ $ignoredServer);
+activatePackage(ServerQueryIgnore);
